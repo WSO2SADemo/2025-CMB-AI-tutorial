a61603eb2cc7e0fec23d8ca1f55aac1748a2bf60
ramindu <ramindus@wso2.com>
2025-09-04 12:58:37 +0530

    add changes to configure configurable variable

--- Lab-02-building-travel-planner/frontend/src/config/auth.js
+++ Lab-02-building-travel-planner/frontend/src/config/auth.js
@@ -1,13 +1,13 @@
 const authConfig = {
-  clientId: process.env.REACT_APP_ASGARDEO_CLIENT_ID || "gN3C2joaMJxlesWx1KpGRejn8Oca",
-  baseUrl: process.env.REACT_APP_ASGARDEO_BASE_URL || "https://api.asgardeo.io/t/nadheesh",
+  clientId: window.REACT_APP_ASGARDEO_CLIENT_ID || "gN3C2joaMJxlesWx1KpGRejn8Oca",
+  baseUrl: window.REACT_APP_ASGARDEO_BASE_URL || "https://api.asgardeo.io/t/nadheesh",
   scopes: ["openid", "profile", "email"],
-  redirectSignIn: process.env.REACT_APP_REDIRECT_SIGN_IN || window.location.origin,
-  redirectSignOut: process.env.REACT_APP_REDIRECT_SIGN_OUT || window.location.origin,
+  redirectSignIn: window.REACT_APP_REDIRECT_SIGN_IN || window.location.origin,
+  redirectSignOut: window.REACT_APP_REDIRECT_SIGN_OUT || window.location.origin,
   storage: "localStorage",
   // This is crucial - must include your hotel API base URL
   resourceServerURLs: [
-    process.env.REACT_APP_HOTEL_API_BASE_URL || "http://localhost:9090"
+    window.REACT_APP_HOTEL_API_BASE_URL || "http://localhost:9090"
   ],
   enablePKCE: true,
   clockTolerance: 300
